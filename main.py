001214bc69d3dbbb464e8be8ef910f5c51d02d30
Davide Pons <davide.pons@zuru.com>
2023-08-07 18:15:00 +0200

    First implementation of Q-value and policy that follows the optimal computed q-value

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ import random as rand
 import numpy as np
 
 import math_utils as mu
-import RL.GymCartPole.policy as sp
+import scaled_ndarray as sa
+
+def update_q_value(q_table, prev_state, action, curr_state, reward, alpha, gamma):    
+    prev_q = q_table[prev_state + (action,)]        # Q-value to be updated
+    curr_q = q_table[curr_state]                    # (Q_value[L], Q_value[R])
+
+    prev_q = (1-alpha)*prev_q + alpha*(reward + gamma*(max(curr_q))) 
+    q_table[prev_state + (action,)] = prev_q
+    return prev_q
+
+def choose_next_action(q_table, curr_state):
+    curr_q = q_table[curr_state]                    # (Q_value[L], Q_value[R])
+    return np.argmax(curr_q)
+
 
 env = gym.make("CartPole-v1", render_mode = "human")
 
@@ -20,14 +33,31 @@ env = TransformObservation(env, lambda obs: (mu.round_to(obs[0], 0.1), mu.round_
 # 1 - right
 action_space = [0, 1]
 
-state_space = sp.Policy((94, 48, 2), (0.1, math.radians(1), 1))
-observation, info = env.reset()
-action = rand.randint(0, 1)
+alpha = 0.5     # Learning rate
+gamma = 0.5       # Discount factor
+
+# Policy and Q-table 
+state_space_shape = (94, 48, 2)
+state_space_scale = (0.1, math.radians(1), 1)
+cart_policy = sa.ScaledNDArray(state_space_shape, state_space_scale)
+q_table = sa.ScaledNDArray(state_space_shape, state_space_scale)
+
+prev_obs, info = env.reset()
+action = env.action_space.sample()
 for _ in range(1000):
-    observation, reward, term, trunc, info = env.step(action)
+    curr_obs, reward, term, trunc, info = env.step(action)
+    print("New observation: " + str(curr_obs))
+    print("Reward: " + str(reward))
 
-    print(observation)
+    new_q_value = update_q_value(q_table=q_table, prev_state=prev_obs, action=action, curr_state=curr_obs, reward=reward, alpha=alpha, gamma=gamma)
+    print("New q value for " + str(prev_obs + (action,)) + " = " + str(new_q_value))
 
+    prev_obs = curr_obs
+    action = choose_next_action(q_table, curr_obs)
+    print("Next action will be: " + "L" if 0 else "R")
+    
     if (term):
+        # todo Update policy with negative reward?
         env.reset()
-        quit()
+
+quit()
--- scaled_ndarray.py
+++ scaled_ndarray.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-class Policy:
+class ScaledNDArray:
     '''
         shape - Tuple representing the shape of the state space (e.g. (4, 4, 2) is a 4x4x2 dimensions array)
         scale_value - Tuple representing the intervals between one value of each dimension and the next one (e.g. [0 0.1 0.2 0.3 ...] has scale_value 0.1)
     '''
     def __init__(self, shape, scale_value):
-        self._state_space = np.ndarray(shape)
+        self.shape = shape
         self._scale_value = scale_value
+        self._state_space = np.ndarray(shape, np.float32)
+
+    '''
+        Get a tuple to index a specific element of this array, and return a tuple with the actual integer indices
+    '''
+    def KeyToIdx(self, key):
+        return tuple(round(k / s) for k, s in zip(key, self._scale_value))
 
     ''' 
         Operator []
@@ -15,5 +22,7 @@ class Policy:
         e.g. (0.1, 5, 2.4) can be used to access a 3-dimensional state-space
     '''
     def __getitem__(self, key):
-        idx = tuple(round(k / s) for k, s in zip(key, self._scale_value))
-        return self._state_space[idx]
+        return self._state_space[self.KeyToIdx(key)]
+    
+    def __setitem__(self, key, value):
+        self._state_space[self.KeyToIdx(key)] = value
